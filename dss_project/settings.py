6623c63f712dd564efcd528b41d583ecd92e5e5a
Cao Vu Thu Nga <cvthng@gmail.com>
2026-04-16 00:05:13 +0700

    add database

--- dss_project/settings.py
+++ dss_project/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-7+wc1-@t%1v_-+y(64r-5jg7nr+9+7-h7bi6&5n$xrshh327bh')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
